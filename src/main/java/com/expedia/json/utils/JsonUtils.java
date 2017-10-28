9b82f58965742d91c8dcd2cb8deae21ffceff07f
shareef <shareef@sajilni.com>
2017-10-28 23:34:57 +0200

    10- Add Json Utils to call Http Request and parse result to json format

---
+++ src/main/java/com/expedia/json/utils/JsonUtils.java
@@ -0,0 +1,84 @@
+package com.expedia.json.utils;
+
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+/**
+ * @author shareef on 27/10/2017
+ *
+ * json utility conversions and other things might be added here
+ */
+public class JsonUtils {
+
+    /**
+     * connect to url and get result as json you can get from it attribute
+     *
+     * @param sURL
+     * @return
+     */
+    public static String getJSONFromUrl(String sURL) {
+        String attribute = null;
+        try {
+            // Connect to the URL using java's native library
+            URL url = null;
+            try {
+                url = new URL(sURL);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(JsonUtils.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            // Convert to a JSON object to print data
+            JsonParser jp = new JsonParser(); //from gson
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+            JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+            attribute = rootobj.toString(); //just grab the zipcode
+        } catch (IOException ex) {
+            Logger.getLogger(JsonUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return attribute;
+    }
+
+    /**
+     * connect to url and get result as json you can get from it attribute
+     *
+     * @param sURL
+     * @param attributeToGet
+     * @return
+     */
+    public static String getAttributeFromJSONUrl(String sURL, String attributeToGet) {
+        String attribute = null;
+        try {
+            // Connect to the URL using java's native library
+            URL url = null;
+            try {
+                url = new URL(sURL);
+            } catch (MalformedURLException ex) {
+                Logger.getLogger(JsonUtils.class.getName()).log(Level.SEVERE, null, ex);
+            }
+            HttpURLConnection request = (HttpURLConnection) url.openConnection();
+            request.connect();
+
+            // Convert to a JSON object to print data
+            JsonParser jp = new JsonParser(); //from gson
+            JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent())); //Convert the input stream to a json element
+            JsonObject rootobj = root.getAsJsonObject(); //May be an array, may be an object.
+            attribute = rootobj.get(attributeToGet).toString(); //just grab the zipcode
+        } catch (IOException ex) {
+            Logger.getLogger(JsonUtils.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        return attribute;
+    }
+
+}
